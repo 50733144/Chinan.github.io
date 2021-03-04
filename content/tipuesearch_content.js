b990683edb7516473cb83ea34c2bf6fafb1f4881
chinan <50733144@gm.nfu.edu.tw>
2021-03-04 21:18:57 +0800

    test

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'TONY', 'text': '\n https://www.youtube.com/watch?v=Lwi5wezOpzA', 'tags': '', 'url': 'TONY.html'}, {'title': 'LabVIEW', 'text': '\n \n https://www.youtube.com/watch?v=2Qs6NPrgcAw&t=84s', 'tags': '', 'url': 'LabVIEW.html'}]};
+var tipuesearch = {"pages": [{'title': 'TONY1', 'text': '\n https://www.youtube.com/watch?v=Lwi5wezOpzA \n', 'tags': '', 'url': 'TONY1.html'}, {'title': 'LabVIEW', 'text': '\n \n https://www.youtube.com/watch?v=2Qs6NPrgcAw&t=84s', 'tags': '', 'url': 'LabVIEW.html'}]};
